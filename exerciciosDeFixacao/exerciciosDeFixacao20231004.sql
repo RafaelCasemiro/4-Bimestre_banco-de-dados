84ed8bfbcbe7c7287ae17dc18e62ce02ebfcaf06
RafaelCasemiro <143537803+RafaelCasemiro@users.noreply.github.com>
2023-10-16 17:18:58 -0300

    Questão 1

--- exerciciosDeFixacao/exerciciosDeFixacao20231004.sql
+++ exerciciosDeFixacao/exerciciosDeFixacao20231004.sql
@@ -1,1 +1,19 @@
+Questão 1
 
+DROP TABLE nomes;
+CREATE TABLE nomes (
+nome VARCHAR(100)
+);
+
+INSERT INTO nomes VALUES
+('Roberta'),
+('Roberto'),
+('Maria Clara'),
+('João');
+
+select * from nomes;
+
+select upper(nome) from nomes;
+select length(nome) from nomes;
+select concat('Sr. ', nome) from nomes where nome like '%o';
+select concat('Sra. ', nome) from nomes where nome like '%a';
